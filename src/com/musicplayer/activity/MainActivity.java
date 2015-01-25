5d299799c3bf0967cca2218687c181dee20abb43
huanghuan123 <18825123778@163.com>
2015-01-25 11:32:58 +0800

    text commit

--- src/com/musicplayer/activity/MainActivity.java
+++ src/com/musicplayer/activity/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
+	private int count;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
     	// TODO Auto-generated method stub
